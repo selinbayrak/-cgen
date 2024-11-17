6ea83afe5d16370da8ad9eb22bb077e5442f63db
AYSE BAYRAK <AYSE BAYRAK@LAPTOP-J2JFAAN1>
2024-11-17 21:49:17 +0300

    Proje dosyası ekle.

---
+++ Ödev3/Program.cs
@@ -0,0 +1,16 @@
+﻿for (int a = 0; a < 21; a++)
+{
+    for (int b = 0; b <= 20 - a; b++)
+    {
+        Console.Write(" ");
+
+    }
+    for (int c = 0; c <= 2 * a; c++)
+    {
+        Console.Write("*");
+
+    }
+
+    Console.WriteLine();
+
+}
